10c608c8a36b0a1d51046c0e84a6eaa01315385c
Deazrdn <deazerlindaandin@gmail.com>
2022-11-08 16:33:29 +0700

    UTS PWEB2

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Welcome to PASWA 2022</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <style type="text/css">
+         html,body{height: 100%;}
+         .bg-utama{
+         background-image: url("https://www.kibrispdr.org/data/330/gambar-gambar-background-55.jpg");
+         background-size: cover;
+         background-position: center;
+         text-align: center;
+         height: 100%;
+         width: 100%;
+         display: table;
+         vertical-align: middle;
+         }
+         h1,p{color: black;}
+         .konten-ditengah {
+         display: table-cell;
+         vertical-align: top;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="bg-utama">
+      <div class="konten-ditengah">
+            <h1>Welcome to PASWA 2022</h1>
+            <p>STIKOM CKI</p>
+            <div class="row justify-content-around">
+            <a class="btn btn-primary" href="login.php">Login</a>
+            <a class="btn btn-primary" href="register.php">Register</a>
+        </div>
+         </div>
+      </div>
+   </body>
+</html>
